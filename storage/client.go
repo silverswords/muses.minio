a658ea5b41d4d62d04cb6d24eb2b0d63823bac9b
Oiar <643921472@qq.com>
2020-06-16 16:22:49 +0800

    decorator

--- storage/client.go
+++ storage/client.go
@@ -20,6 +20,13 @@ type client struct {
 
 type clientOption func(*client)
 
+var defaultClientOptions = client{
+	Endpoint:        "127.0.0.1:9001",
+	AccessKeyID:     "minio",
+	SecretAccessKey: "minio123",
+	UseSSL:          false,
+}
+
 func WithEndpoint(e string) clientOption {
 	return func(c *client) {
 		c.Endpoint = e
@@ -54,13 +61,6 @@ func (c *client) GetMinioClient() *minio.Client {
 	return c.NewMinioClient
 }
 
-var defaultClientOptions = client{
-	Endpoint:        "127.0.0.1:9001",
-	AccessKeyID:     "minio",
-	SecretAccessKey: "minio123",
-	UseSSL:          false,
-}
-
 func NewClient(options client, newMinioClient *minio.Client) Client {
 	return &client{
 		Endpoint:        options.Endpoint,
@@ -71,12 +71,7 @@ func NewClient(options client, newMinioClient *minio.Client) Client {
 	}
 }
 
-func NewMinioClient(opts ...clientOption) Client {
-	options := defaultClientOptions
-	for _, o := range opts {
-		o(&options)
-	}
-
+func NewMinioClient(options client) Client {
 	newMinioClient, err := minio.New(options.Endpoint, options.AccessKeyID, options.SecretAccessKey, options.UseSSL)
 	if err != nil {
 		fmt.Println(err)
@@ -84,4 +79,12 @@ func NewMinioClient(opts ...clientOption) Client {
 	}
 	minioClient := NewClient(options, newMinioClient)
 	return minioClient
+}
+
+func DecoratorNewMinioClient(opts ...clientOption) Client {
+	options := defaultClientOptions
+	for _, o := range opts {
+		o(&options)
+	}
+	return NewMinioClient(options)
 }
